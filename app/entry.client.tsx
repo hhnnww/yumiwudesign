995a80ed2e43cfe56a729dd5623fc4f3eb24ef6b
weihua <175990271@qq.com>
2024-04-30 09:25:20 +0800

    12313123

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -11,6 +11,8 @@ interface ClientCacheProviderProps {
 }
 
 function ClientCacheProvider({ children }: ClientCacheProviderProps) {
+  localStorage.clear();
+
   const [cache, setCache] = React.useState(createEmotionCache());
 
   const reset = React.useCallback(() => {
